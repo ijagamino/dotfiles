6854d691be8f9f1fc71a9ab06be9b23a4ee05ceb
Ivan <ijagamino@gmail.com>
2024-07-04 20:50:06 +0800

    feat: add keybinds for terminal panes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,19 +1,62 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+local act = wezterm.action
 
--- For example, changing the color scheme:
+-- This is where you actually apply your config choices
+-- Wezterm window configuration
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 
-config.color_scheme = 'Catppuccin Mocha'
-
+-- Wezterm font configuration
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 16
 
+-- Color scheme configuration
+config.color_scheme = "Catppuccin Mocha"
+
+-- Keybinding configuration
+config.keys = {
+	-- Pane control
+	{
+		key = "w",
+		mods = "ALT",
+		action = act.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "o",
+		mods = "ALT",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "v",
+		mods = "ALT",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	-- Pane movement
+	{
+		key = "j",
+		mods = "ALT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "k",
+		mods = "ALT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "h",
+		mods = "ALT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+}
 -- and finally, return the configuration to wezterm
 return config

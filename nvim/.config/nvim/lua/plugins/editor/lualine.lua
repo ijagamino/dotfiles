aea17de30299b2d25a89a96bad6d739a2d2756cd
ijagamino <ijagamino@gmail.com>
2024-07-16 01:18:15 +0800

    feat: rice lualine

--- nvim/.config/nvim/lua/plugins/editor/lualine.lua
+++ nvim/.config/nvim/lua/plugins/editor/lualine.lua
@@ -14,10 +14,17 @@ return {
 				-- globalstatus = true,
 			},
 			sections = {
-				lualine_a = { "branch" },
-				lualine_b = { "diff", "diagnostics" },
+				lualine_a = {
+					{
+						"mode",
+						fmt = function(str)
+							return str:sub(1, 1)
+						end,
+					},
+				},
+				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
-				lualine_x = {},
+				lualine_x = { "filetype" },
 				lualine_y = {},
 				lualine_z = {},
 			},

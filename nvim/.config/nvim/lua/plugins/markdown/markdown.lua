20e28483a76abe961c8bcae8608077d51dedf55f
ijagamino <ijagamino@gmail.com>
2024-08-04 15:04:28 +0800

    feat: add markdown.lua

---
+++ nvim/.config/nvim/lua/plugins/markdown/markdown.lua
@@ -0,0 +1,7 @@
+return {
+	"MeanderingProgrammer/markdown.nvim",
+	main = "render-markdown",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	opts = {},
+	name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+}

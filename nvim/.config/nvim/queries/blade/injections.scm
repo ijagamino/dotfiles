b817fd7eb8f495f92ac15085629b0d51b3ba9cd2
ijagamino <ijagamino@gmail.com>
2024-08-05 18:04:27 +0800

    feat: laravel blade stuff

--- nvim/.config/nvim/queries/blade/injections.scm
+++ nvim/.config/nvim/queries/blade/injections.scm
@@ -3,6 +3,9 @@
     (#set! injection.combined)
     (#set! injection.language php))
 
+((php_only) @injection.content
+   (#set! injection.language php_only))
+
 ((text) @injection.content
     (#has-ancestor? @injection.content "envoy")
     (#set! injection.combined)

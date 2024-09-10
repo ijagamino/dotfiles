2871636bcdca004d42040071c4ead6399b235bd9
ivan <ijagamino@gmail.com>
2024-09-10 20:19:38 +0800

    feat: add codeium keybind

--- nvim/.config/nvim/lua/plugins/editor/codeium.lua
+++ nvim/.config/nvim/lua/plugins/editor/codeium.lua
@@ -1,4 +1,11 @@
 return {
 	"Exafunction/codeium.vim",
 	event = "BufEnter",
+
+	config = function()
+		-- Change '<C-g>' here to any keycode you like.
+		vim.keymap.set("i", "<C-g>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+	end,
 }

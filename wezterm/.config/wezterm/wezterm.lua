5066b7b3993ab6e10954b3404ad7773104a64229
Ivan Joshua Gamino <ijagamino@gmail.com>
2025-12-30 19:39:59 +0800

    update hyprland config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("DepartureMono Nerd Font Mono")
+config.color_scheme = "Gruvbox Material (Gogh)"
+config.font_size = 14.0
+config.enable_tab_bar = false
+
+return config

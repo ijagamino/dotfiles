e42ac9a05ff0bbb236a94c532fb6d02487b8d64a
Ivan Joshua A. Gamino <ijagamino@gmail.com>
2026-08-13 21:41:14 +0800

    feat(hypr): add gnome keyring daemon to startup

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -23,7 +23,7 @@ hl.on("hyprland.start", function()
 	--   hl.exec_cmd(terminal)
 	--   hl.exec_cmd("nm-applet")
 	-- hl.exec_cmd("waybar & hyprpaper & firefox")
-	hl.exec_cmd("waybar & hyprpaper")
+	hl.exec_cmd("waybar & hyprpaper & gnome-keyring-daemon --start --components=secrets")
 end)
 
 -------------------------------

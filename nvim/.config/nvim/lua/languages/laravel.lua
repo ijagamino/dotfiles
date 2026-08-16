1a29c749c8a0a73e7891179c8250be15aeda49b0
Ivan Joshua Gamino <ijagamino@ijagamino.localdomain>
2026-08-17 02:17:08 +0800

    fix(nvim): laravel lsp starting on non-laravel projects

--- nvim/.config/nvim/lua/languages/laravel.lua
+++ nvim/.config/nvim/lua/languages/laravel.lua
@@ -1,1 +1,23 @@
-vim.lsp.enable("laravel_ls")
+-- Only start LSP on Laravel projects (has "artisan")
+-- vim.lsp.enable("laravel_ls") starts LSP even before checking for root marker ("artisan")
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "php",
+	callback = function(args)
+		local root = vim.fs.root(args.buf, "artisan")
+		if not root then
+			return
+		end
+
+		local base_config = vim.lsp.config.laravel_ls
+
+		if not base_config then
+			vim.notify("laravel_ls config not found", vim.log.levels.WARN)
+			return
+		end
+
+		local config = vim.deepcopy(base_config)
+		config.root_dir = root
+
+		vim.lsp.start(config, { bufnr = args.buf })
+	end,
+})
